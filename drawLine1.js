104a28af5eda8c21e4d83c153c1136aaeb73e031
jonodeno <jondenose@gmail.com>
2018-02-18 19:26:15 -0600

    Trends page updates
    Suppressed icons from Plotly for all plots
    Added y axis labels
    Added plot titles
    Changed y axis numbers to make more sense

--- drawLine1.js
+++ drawLine1.js
@@ -2,11 +2,17 @@ var month = ["Week 1", "Week 2", "Week 3", "Week 4"];
 
 var trace1 = {
   x: month,
-  y: [10, 15, 13, 17],
+  y: [1000, 1500, 1300, 1700],
   type: 'scatter'
 };
 
+var layout = {
+  title:"This Month's Water Usage",
+  yaxis:{
+    title:'Liters'
+  }
+};
 
 var data = [trace1];
 
-Plotly.newPlot('trend_water', data);
+Plotly.newPlot('trend_water', data, layout);
