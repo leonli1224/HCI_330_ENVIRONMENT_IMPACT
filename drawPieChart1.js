e3d07a157cd5b7ef763da5825d770c3c26dbad85
jonodeno <jondenose@gmail.com>
2018-02-22 23:48:56 -0600

    Font styling
    Made fonts consistent within plots and the website as a whole
    Added what will beome the redesigned homepage

--- drawPieChart1.js
+++ drawPieChart1.js
@@ -5,7 +5,21 @@ var data = [{
 }];
 
 var layout = {
-  title:'How Do I Use Water?'
+  height: 400,
+  width: 500,
+  title:'How Do I Use Water? My Overall Water Trend',
+  titlefont:{
+    family: 'Avenir, sans-serif',
+    size: 18
+  },
+  legend: {
+    x: 1,
+    y: 1,
+    font:{
+      family: 'Avenir, sans-serif',
+      size: 14
+    }
+  }
 };
 
 Plotly.newPlot('pie_water', data, layout, {displayModeBar: false});
